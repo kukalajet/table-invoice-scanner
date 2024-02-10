b0cd55af301a5029374ea6697e5ea8c21948917e
Jeton Kukalaj <jeton.kukalaj@gmail.com>
2024-02-10 10:34:35 +0100

    Remove unused files and update index page

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const IndexPage: React.FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello, World!</Text>
+    </View>
+  );
+};
+
+export default IndexPage;
